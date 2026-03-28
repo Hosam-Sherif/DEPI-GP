2edb34f91014497207cbe682234f1ca77ab4245a
Mohame-Amin16 <dev.mohameamin16@gmail.com>
2026-03-28 07:14:40 +0200

    Connection String

--- Mazaad.API/Program.cs
+++ Mazaad.API/Program.cs
@@ -13,6 +13,7 @@ namespace Mazaad.API
 
             // Add services to the container.
 
+            
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
